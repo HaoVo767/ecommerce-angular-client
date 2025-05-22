2bbc6f7dba2fa4f585639467f434643f91d8f819
minh.hao <minhhao767@gmail.com>
2025-05-23 02:25:50 +0700

    update server url

--- src/env/env.ts
+++ src/env/env.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 export class environment {
   production = true;
   // apiUrl = 'http://localhost:3001/api';
-  apiUrl = 'https://ecommerce-backend-nestjs-production.up.railway.app/api';
+  apiUrl = 'https://ecommerce-nestjs-deploy-to-vercel.onrender.com/api';
   storeId = '3cd54769-7e3f-4b8b-8847-b6b6f8e6b618';
   baseUrl() {
     return this.apiUrl;
